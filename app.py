3e2419abcc2cf948f46dce53675e3265419eaab3
procoder2009 <aashibubna31@gmail.com>
2022-07-11 17:48:51 +0530

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
 
+
+
 app = Flask(__name__)
 
 # Open and redirect to default upload webpage
@@ -12,38 +14,32 @@ app = Flask(__name__)
 def load_form():
     return render_template('upload.html')
 
-
 # Function to upload image and redirect to new webpage
 @app.route('/gray', methods=['POST'])
 def upload_image():
     file = request.files['file']
     filename = secure_filename(file.filename)
-
-    file_data = make_grayscale(file.read())
+    # write the read and write function on image below 
+    file_data= make_greyscale(file.read())
     with open(os.path.join('static/', filename),
-              'wb') as f:
-        f.write(file_data)
+    	'wb') as f:
+    	f.write(file_data)   
 
-    display_message = 'Image successfully uploaded and displayed below'
+
+    display_message = 'image successfully uploaded and displayed below'
     return render_template('upload.html', filename=filename, message = display_message)
 
+def make_greyscale(input_image):
+	image_array= np.fromstring(input_image, dtype= 'uint8')
+	print('Image Array:', image_array)
 
+	decode_array_to_img= cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
+	print('decode image values: ', decode_array_to_img)
 
-def make_grayscale(input_image):
+	converted_gray_img= cv2.cvtColor(decode_array_to_img, cv2.COLOR_BGR2GRAY)
+	status, output_image= cv2.imencode('.PNG', converted_gray_img)
 
-    image_array = np.fromstring(input_image, dtype='uint8')
-    print('Image Array:',image_array)
-
-    # decode the array into an image
-    decode_array_to_img = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
-    print('Decode values of Image:', decode_array_to_img)
-
-    # Make grayscale
-    converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2GRAY)
-    status, output_image = cv2.imencode('.PNG', converted_gray_img)
-    print('Status:',status)
-
-    return output_image
+	return output_image
 
 
 @app.route('/display/<filename>')
